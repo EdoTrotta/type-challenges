29e6670ed72b07f99c0e4f696abc4bf8dfafee66
edoardo@fxdigital.uk <edoardo@fxdigital.uk>
2022-10-21 14:25:59 +0100

    easy-tuple-to-object: iterate over T[number] and assign

--- questions/00011-easy-tuple-to-object/template.ts
+++ questions/00011-easy-tuple-to-object/template.ts
@@ -1,1 +1,4 @@
-type TupleToObject<T extends readonly any[]> = any
+// iterate over T[number] and return a new type
+// where the key and value is the type from T[number]
+
+type TupleToObject<T extends readonly PropertyKey[]> = { [K in T[number]]: K }
